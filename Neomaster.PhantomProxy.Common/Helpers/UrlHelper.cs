54866e70d6347decd53c5c67a7ff3d292a757a45
Neomaster X <neomasterx@outlook.com>
2025-08-14 22:48:49 +0300

    refactor: #1 UrlHelper.TryCreateUri()

--- Neomaster.PhantomProxy.Common/Helpers/UrlHelper.cs
+++ Neomaster.PhantomProxy.Common/Helpers/UrlHelper.cs
@@ -5,4 +5,18 @@ namespace Neomaster.PhantomProxy.Common;
 /// </summary>
 public static class UrlHelper
 {
+  /// <summary>
+  /// Creates <see cref="Uri"/> from string URL.
+  /// </summary>
+  /// <param name="url">Absolute or relative string URL.</param>
+  /// <returns><see cref="Uri"/> if given string URL is valid, otherwise <c>null</c>.</returns>
+  public static Uri? TryCreateUri(string url)
+  {
+    if (Uri.TryCreate(url, UriKind.RelativeOrAbsolute, out var uri))
+    {
+      return uri;
+    }
+
+    return null;
+  }
 }
