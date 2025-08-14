ed5bd4f76c4c337767c427a1861798831e1cab9a
Neomaster X <neomasterx@outlook.com>
2025-08-14 23:52:06 +0300

    tests: #1 UrlHelper.CreateUri(url)

--- Neomaster.PhantomProxy.Common.UnitTests/UrlHelperUnitTests.cs
+++ Neomaster.PhantomProxy.Common.UnitTests/UrlHelperUnitTests.cs
@@ -30,6 +30,33 @@ public class UrlHelperUnitTests
   }
 
   [Theory]
+  [InlineData("https://example.com")]
+  [InlineData("https://example.com/")]
+  [InlineData(" https://example.com ")]
+  [InlineData("rel/path")]
+  [InlineData("rel/path/")]
+  [InlineData("/rel/path")]
+  [InlineData("/rel/path/")]
+  [InlineData(" rel/path ")]
+  public void CreateUri_ShouldReturnUri_ValidUrl(string url)
+  {
+    var expectedUri = UrlHelper.TryCreateUri(url);
+
+    var uri = UrlHelper.CreateUri(url);
+
+    Assert.NotNull(expectedUri);
+    Assert.Equal(expectedUri, uri);
+  }
+
+  [Theory]
+  [InlineData("")]
+  [InlineData(" ")]
+  public void CreateUri_ShouldThrow_InvalidUrl(string url)
+  {
+    Assert.Throws<ArgumentException>(() => UrlHelper.CreateUri(url));
+  }
+
+  [Theory]
   [InlineData("https://a", "https://b", "https://a")]
   [InlineData("a", "https://b", "https://b/a")]
   public void ToAbsolute_ShouldReturnAbsoluteUri(string url, string baseUrl, string expectedUrl)
