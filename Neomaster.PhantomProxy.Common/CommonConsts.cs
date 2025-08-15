926931badcff68cd78f123a7d42804084127a601
Neomaster X <neomasterx@outlook.com>
2025-08-15 23:07:53 +0300

    feat: #14 Regular expression pattern for matching url() function

--- Neomaster.PhantomProxy.Common/CommonConsts.cs
+++ Neomaster.PhantomProxy.Common/CommonConsts.cs
@@ -6,6 +6,11 @@ namespace Neomaster.PhantomProxy.Common;
 public class CommonConsts
 {
   /// <summary>
+  /// Regular expression pattern for matching <c>url()</c> function.
+  /// </summary>
+  public const string UrlFunctionRegexPattern = @"url\(\s*(?:(['""])(?<url>.*?)\1|(?<url>[^)]+))\s*\)";
+
+  /// <summary>
   /// XML structure attribute names.
   /// </summary>
   public class XmlAttributeNames
