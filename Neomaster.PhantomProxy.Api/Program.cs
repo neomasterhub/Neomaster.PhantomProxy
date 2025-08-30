49662817916c50a927f84a35d67b324212bef124
neomaster <neomasterx@outlook.com>
2025-08-30 11:15:53 +0300

    #37 Set forwarded headers before middlewares.

--- Neomaster.PhantomProxy.Api/Program.cs
+++ Neomaster.PhantomProxy.Api/Program.cs
@@ -22,16 +22,16 @@ builder.Services.AddScoped<IUrlEncryptService, UrlAesEncryptService>();
 builder.Services.AddMemoryCache();
 
 var app = builder.Build();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+  ForwardedHeaders = ForwardedHeaders.XForwardedProto | ForwardedHeaders.XForwardedFor,
+});
 app.Use(async (context, next) =>
 {
   context.Response.Headers.CacheControl = "no-store, no-cache, must-revalidate, max-age=0";
   context.Response.Headers.Pragma = "no-cache";
   context.Response.Headers.Expires = "0";
   await next();
-});
-app.UseForwardedHeaders(new ForwardedHeadersOptions
-{
-  ForwardedHeaders = ForwardedHeaders.XForwardedProto,
 });
 app.UseStaticFiles();
 app.UseSwagger();
